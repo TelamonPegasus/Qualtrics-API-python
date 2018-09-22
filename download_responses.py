f6fb3fdc8a9931482415b92eb838fc40a6c845ff
TelamonPegasus <telamon025115@gmail.com>
2018-09-21 22:44:48 -0600

    added GET call for surveyIDs from the project library

--- download_responses.py
+++ download_responses.py
@@ -2,6 +2,7 @@ import os
 import sys
 import requests, zipfile, io
 import json
+import time
 
 apiToken = os.environ["Q_API_TOKEN"]     
 dataCenter = os.environ["Q_DATA_CENTER"] 
@@ -16,15 +17,18 @@ headers = {
 
 def download_responses(surveyId, format_type):
 	"""
-	download all responses from a survey
+	Download all responses from a survey
+
+	format_type can be csv, json, or spss
 	"""
 
-	data = '{{"surveyId": "{0}", "format": "{1}"}}'.format(surveyId, format_type)
+	# data = '{{"surveyId": "{0}", "format": "{1}"}}'.format(surveyId, format_type)
+	data = '{{"surveyId": {0}, "format": "{1}"}}'.format(surveyId, format_type)
 	baseUrl = 'https://{0}.qualtrics.com/API/v3/responseexports'.format(dataCenter)
 	response = requests.post(baseUrl, headers=headers, data=data)
 	
 	if response.status_code != 200:
-		print ('error making post request to initate download -- aborted')
+		print ('error making post request to initate download for survey {} -- aborted'.format(surveyId))
 		return
 
 	print ('successfully sent request to Qualtrics servers')
@@ -44,9 +48,12 @@ def download_responses(surveyId, format_type):
 		if status == 'complete':
 			done = True
 
-		if status == "cancelled" or status == "failed":
+		elif status == "cancelled" or status == "failed":
 			print("error while generating download -- aborted")
 			return
+
+		else:
+			time.sleep(5)
 
 
 	print ("\nsuccessfully generated file, attempting to download...")
@@ -69,10 +76,47 @@ def download_responses(surveyId, format_type):
 	return
 
 
+def list_all_surveys():
+	"""
+	Get the Survey IDs of all surveys in your account
+
+	Returns an array of SurveyIds
+	"""
+	baseUrl = 'https://{0}.qualtrics.com/API/v3/surveys'.format(dataCenter)
+	response = requests.get(baseUrl, headers=headers)
+
+	if response.status_code != 200:
+		print ('error making post request to initate download -- aborted')
+		return
+
+	ids = []
+	for element in response.json()['result']['elements']: ids.append("{}".format(element['id']))
+
+	nextPage = response.json()['result']['nextPage']
+
+	while nextPage != None:
+
+		response = requests.get(nextPage, headers=headers)
+
+		if response.status_code != 200:
+			print ('error making post request to initate download -- aborted')
+			return
+
+		nextPage = response.json()['result']['nextPage']
+		for element in response.json()['result']['elements']: ids.append(element['id'])
+
+	print (ids)
+
+	return ids
 
 
-download_responses("SV_eDHWohdKuzG2245", "json")
 
+
+surveyIds = list_all_surveys()
+print ('found {} total surveys in your library'.format(len(surveyIds)))
+for surveyId in surveyIds:
+	double_quotes_id = '"' + surveyId + '"' 	# necessary for JSON
+	download_responses(double_quotes_id, "csv")
 
 
 
